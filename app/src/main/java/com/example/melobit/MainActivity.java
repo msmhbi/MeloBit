814cb19ed814c0a8c9f5e6054308bf510112ed92
haniezibadel <hanie.zibadel82@gmail.com>
2023-01-20 00:30:09 +0330

    Show results returned by server in main activity

--- app/src/main/java/com/example/melobit/MainActivity.java
+++ app/src/main/java/com/example/melobit/MainActivity.java
@@ -1,24 +1,23 @@
 package com.example.melobit;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.melobit.adapter.ArtistAdapter;
+import com.example.melobit.adapter.SongAdapter;
+import com.example.melobit.models.ArtistsResponse;
+import com.example.melobit.models.SongsResponse;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity{
 
-    private TextView res;
+    private TextView error;
     private RecyclerView newSongs, TopSinger;
-    private EditText etQuery;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,13 +25,11 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        res = findViewById(R.id.res);
+        error = findViewById(R.id.error);
         newSongs = findViewById(R.id.new_songs);
         TopSinger = findViewById(R.id.top_singers);
-        Button hitsBtn = findViewById(R.id.btn);
+        FloatingActionButton hitsBtn = findViewById(R.id.btn);
         FloatingActionButton btnSearch = findViewById(R.id.btn_search);
-        etQuery = findViewById(R.id.et_query);
-
 
         LinearLayoutManager layoutManager
                 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
@@ -41,6 +38,37 @@ public class MainActivity extends AppCompatActivity{
                 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         TopSinger.setLayoutManager(layoutManager1);
 
+        hitsBtn.setOnClickListener(view -> {
+            //todo
+        });
+        btnSearch.setOnClickListener(view -> {
+            //todo
+        });
 
+        RequestManager manager = new RequestManager();
+
+        SongsRequestListener latestListener = new SongsRequestListener() {
+            @Override
+            public void didFetch(SongsResponse response) {
+                newSongs.setAdapter(new SongAdapter(MainActivity.this, response.getResults()));
+            }
+            @Override
+            public void didError(String errorMessage) {
+                error.setText(errorMessage);
+            }
+        };
+
+        ArtistsRequestListener artistsListener = new ArtistsRequestListener() {
+            @Override
+            public void didFetch(ArtistsResponse response) {
+                TopSinger.setAdapter(new ArtistAdapter(MainActivity.this, response.getResults()));
+            }
+            @Override
+            public void didError(String errorMessage) {
+                error.setText(errorMessage);
+            }
+        };
+        manager.getLatestSongs(latestListener);
+        manager.getTopSingers(artistsListener);
     }
 }
